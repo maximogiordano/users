0438a13ea0169f9d35c5d1f6e3a4d0eaff660770
Maximo Giordano <maximo.giordano@globallogic.com>
2025-02-07 22:41:11 -0300

    fix constraints

--- src/main/java/com/maximogiordano/users/validation/Password.java
+++ src/main/java/com/maximogiordano/users/validation/Password.java
@@ -4,12 +4,9 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {})
 @Pattern(regexp = "[^A-Z]*[A-Z][^A-Z]*", message = "must contain exactly one uppercase letter")
